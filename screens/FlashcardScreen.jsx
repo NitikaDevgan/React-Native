f6cd6ba7bb3a9e032fa3aaed3a1f605130e85740
unknown <nitikadevganisnd@gmail.com>
2025-06-13 22:33:17 +0530

    Some minor change

--- screens/FlashcardScreen.jsx
+++ screens/FlashcardScreen.jsx
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    padding: 20,
+    padding: 22,
   },
   heading: {
     fontSize: 24,
